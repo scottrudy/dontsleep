0bcbe8ca46b2584b92c56a10b5c0358f1b518adc
Scott Rudy <scottrudy@yahoo.com>
2020-06-10 11:09:16 -0400

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace DontSleep {
+    public class Program {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto,SetLastError = true)]
+        private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint {
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002
+        }
+
+        private static void Main(string[] args) {
+            var timer = new Timer((s) => {
+                SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+            }, null, TimeSpan.Zero, TimeSpan.FromMinutes(9));
+
+            Console.WriteLine($"Tap any key to stop program!");
+            Console.ReadKey();
+            timer.Dispose();
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+    }
+}
